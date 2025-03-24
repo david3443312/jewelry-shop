b97abfcbd71723d85704222ebe306c52426afcd0
david3443312 <pardox2k4@gmail.com>
2025-03-24 23:55:37 +0700

    Update register.php

--- public/assets/admin panel/register.php
+++ public/assets/admin panel/register.php
@@ -1,9 +1,46 @@
+<?php 
+    include '../components/connect.php';
+
+    if (isset($_POST['submit'])) {
+
+        $id = unique_id();
+        $name = $_POST['name'];
+        $name = filter_var($name, FILTER_SANITIZE_SPECIAL_CHARS);
+
+        $pass = sha1($_POST['pass']);
+        $pass = filter_var($pass, FILTER_SANITIZE_SPECIAL_CHARS);
+
+        $email = $_POST['email'];
+        $email = filter_var($email, FILTER_SANITIZE_EMAIL);
+
+        $phone = $_POST['phone'];
+        $phone = filter_var($phone, FILTER_SANITIZE_SPECIAL_CHARS);
+
+        $cpass = sha1($_POST['cpass']);
+        $cpass = filter_var($cpass, FILTER_SANITIZE_SPECIAL_CHARS);
+
+        $select = $conn->prepare("SELECT * FROM `vendors` WHERE email = ?");
+        $select->execute([$email]);
+
+        if ($select->rowCount() > 0) {
+            $warning_msg[] = "Email already exists";
+        } else {
+            if ($pass != $cpass) {
+               $warning_msg[] = "Password does not match";
+            } else {
+                $insert_vendor = $conn->prepare("INSERT INTO `vendors`(`id`, `name`, `password`, `email`, `phone` ) VALUES (?,?,?,?,?)");
+                $insert_vendor->execute([$id, $name, $cpass, $email, $phone]);
+                $success_msg[] = "Registration successful";
+            }
+        }
+    }
+?>
 <!DOCTYPE html>
 <html lang="vi">
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Đăng nhập - Jewelry Shop</title>
+        <title>Đăng ký - Jewelry Shop</title>
         <link rel="stylesheet" href="../css//stylessignup.css">
         <link rel="stylesheet" href="../css//admin_style.css">
         <link rel="stylesheet" href="../css//styleshomepage.css">
@@ -19,45 +56,49 @@
             <a href="#"><i class="fas fa-home"></i> / Đăng ký tài khoản</a>
         </div> -->
         <div class="meetings">
-            <h1>Chào mừng quý khách!</h1>
+            <h1>Xin chào!</h1>
             <h2>Vui lòng đăng ký tài khoản mới tại đây:</h2>
         </div>
-        <form class="signup-form">
+        <form method="post" class="signup-form">
             <div class="form-row">
                 <div class="form-group">
-                    <label for="fullname" al>Họ và tên:</label>
-                    <input type="text" id="fullname" name="fullname" title="Đừng quên nhập họ tên nha!" required>
-                </div>
-                <div class="form-group">
                     <label for="username">Tên đăng nhập:</label>
-                    <input type="text" id="username" name="username" title="Tên đăng nhập phải từ 6-20 ký tự!" pattern="^[a-zA-Z0-9]{6,20}$" required>
+                    <input type="text" required name="name" placeholder="Tên đăng nhập">
                 </div>
                 <div class="form-group">
                     <label for="password">Mật khẩu:</label>
-                    <input type="password" id="password" name="password" title="Mật khẩu phải từ 8-20 ký tự!" pattern=".{8,20}" required>
+                    <input type="password" required name="pass" placeholder="Mật khẩu">
                 </div>
             </div>
             <div class="form-row">
                 <div class="form-group">
                     <label for="email">Email:</label>
-                    <input type="email" id="email" name="email" required>
+                    <input type="email" required name="email" placeholder="Email">
                 </div>
                 <div class="form-group">
                     <label for="phone">Số điện thoại:</label>
-                    <input type="tel" id="phone" name="phone" pattern="^0[0-9]{9,10}$" required>
+                    <input type="tel" required name="phone" placeholder="Số điện thoại">
                 </div>
                 <div class="form-group">
                     <label for="confirm-password">Nhập lại mật khẩu:</label>
-                    <input type="password" id="confirm-password" name="confirm-password">
+                    <input type="password" required name="cpass" placeholder="Nhập lại mật khẩu">
                 </div>
             </div>
             <div class="form-row">
-                <button type="submit" class="signup-btn">Đăng ký</button>
+                <button type="submit" name="submit" class="signup-btn">Đăng ký</button>
             </div>
         </form>
-        
+        <div class="login-link">
+            <h2>Bạn đã có tài khoản:</h2>
+            <a href="login.php">Đăng nhập</a>
+        </div>
         <!-- <img src="images/signup/signup1.jpg" alt="Đăng ký tài khoản" class="signup-image"> -->
     </main>
+
+    <script src = "https://cdnjs.cloudflare.com/ajax/libs/sweetalert/2.1.2/sweetalert.min.js"></script>
+
+         <!-- sweetalert cdn link -->
+    <?php include '../components/alert.php' ?>
     
 </body>
 </html>
