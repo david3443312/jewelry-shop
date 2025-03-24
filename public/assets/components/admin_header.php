ca7aa0dd24cd9041c9302732dc7a92c0efbcde5d
david3443312 <pardox2k4@gmail.com>
2025-03-24 23:55:48 +0700

    Update admin_header.php

--- public/assets/components/admin_header.php
+++ public/assets/components/admin_header.php
@@ -1,38 +1,42 @@
 <header>
     <nav>
         <img src="../images/logoicon.png" alt="Brand logo" class="logo" style = "width: 50px; height: 50px;">
+        <button id="toggle-sidebar"><i class="fa-solid fa-bars"></i></button>
+
         <div class="icons">
             <div class="account-container">
                 <div class="account-icon">
-                    <a href="signup.html"><span class="iconify" data-icon="codicon:account" style="height: 95%; width: 95%;"></span></a>
+                    <a href="profile.php"><span class="iconify" data-icon="codicon:account" style="height: 95%; width: 95%;"></span></a>
                 </div>
                 <div class="dropdown-menu">
                     <div class="box-menu">
-                        <a href="signup.html">Hồ sơ</a>
-                        <a href="signup.html">Thoát</a>
+                        <a href="profile.php">Hồ sơ</a>
+                        <a href="../components/admin_logout.php">Thoát</a>
                     </div>
                     
                 </div>
             </div>
         </div>
         
-        <div class="sidebar">
+        <div class="sidebar" id="sidebar">
             <div class="logo">
                 <img src="../images/logoicon.png" class="logo-img">
             </div>
-            <h2>Trang trại 2F</h2>
+            <h2>Jewelry - Shop</h2>
             <div class="close" id="close-btn">
                 <span class="material-icons-sharp"></span>
             </div>
-            <a href="dashboard.php" class="active db-decorate">
+            <?php $current_page = basename($_SERVER['PHP_SELF']); ?>
+
+            <a href="dashboard.php" class="db-decorate <?php echo ($current_page == 'dashboard.php') ? 'active' : ''; ?>">
                 <span class="material-icons-sharp">grid_view</span>
                 <h4>Dashboard</h4>
             </a>
-            <a href="user_account.php" class="db-decorate">
+            <a href="user_accounts.php" class="db-decorate <?php echo ($current_page == 'user_accounts.php') ? 'active' : ''; ?>">
                 <span class="material-icons-sharp">person_outline</span>
                 <h4>Customers</h4>                  
             </a>
-            <a href="admin_order.php" class="db-decorate">
+            <a href="admin_order.php" class="db-decorate <?php echo ($current_page == 'admin_order.php') ? 'active' : ''; ?>">
                 <span class="material-icons-sharp">receipt_long</span>
                 <h4>Orders</h4>
             </a>
@@ -40,11 +44,11 @@
                 <span class="material-icons-sharp">insights</span>
                 <h4>Analytics</h4>
             </a>
-            <a href="admin_message.php" class="db-decorate">
+            <a href="admin_message.php" class="db-decorate <?php echo ($current_page == 'admin_message.php') ? 'active' : ''; ?>">
                 <span class="material-icons-sharp">mail_outline</span>
                 <h4>Messages</h4>
             </a>
-            <a href="view_product.php" class="db-decorate">
+            <a href="view_product.php" class="db-decorate <?php echo ($current_page == 'view_product.php') ? 'active' : ''; ?>">
                 <span class="material-icons-sharp">inventory</span>
                 <h4>Products</h4>
             </a>
@@ -56,7 +60,7 @@
                 <span class="material-icons-sharp">settings</span>
                 <h4>Settings</h4>
             </a>
-            <a href="add_products.php" class="db-decorate">
+            <a href="add_products.php" class="db-decorate <?php echo ($current_page == 'add_products.php') ? 'active' : ''; ?>">
                 <span class="material-icons-sharp">add</span>
                 <h4>Add Products</h4>
             </a>
