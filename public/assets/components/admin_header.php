3917d016b42ef71011273e9ae7efeb82dbd2c13d
david3443312 <pardox2k4@gmail.com>
2025-03-24 16:37:21 +0700

    Create admin_header.php

---
+++ public/assets/components/admin_header.php
@@ -0,0 +1,65 @@
+<header>
+    <nav>
+        <img src="../images/logoicon.png" alt="Brand logo" class="logo" style = "width: 50px; height: 50px;">
+        <div class="icons">
+            <div class="account-container">
+                <div class="account-icon">
+                    <a href="signup.html"><span class="iconify" data-icon="codicon:account" style="height: 95%; width: 95%;"></span></a>
+                </div>
+                <div class="dropdown-menu">
+                    <a href="signup.html">Đăng ký</a>
+                </div>
+            </div>
+        </div>
+        
+        <div class="sidebar">
+            <div class="logo">
+                <img src="../images/logoicon.png" class="logo-img">
+            </div>
+            <h2>Trang trại 2F</h2>
+            <div class="close" id="close-btn">
+                <span class="material-icons-sharp"></span>
+            </div>
+            <a href="dashboard.php" class="active db-decorate">
+                <span class="material-icons-sharp">grid_view</span>
+                <h4>Dashboard</h4>
+            </a>
+            <a href="user_account.php" class="db-decorate">
+                <span class="material-icons-sharp">person_outline</span>
+                <h4>Customers</h4>                  
+            </a>
+            <a href="admin_order.php" class="db-decorate">
+                <span class="material-icons-sharp">receipt_long</span>
+                <h4>Orders</h4>
+            </a>
+            <a href="#" class="db-decorate">
+                <span class="material-icons-sharp">insights</span>
+                <h4>Analytics</h4>
+            </a>
+            <a href="admin_message.php" class="db-decorate">
+                <span class="material-icons-sharp">mail_outline</span>
+                <h4>Messages</h4>
+            </a>
+            <a href="view_product.php" class="db-decorate">
+                <span class="material-icons-sharp">inventory</span>
+                <h4>Products</h4>
+            </a>
+            <a href="#" class="db-decorate">
+                <span class="material-icons-sharp">report</span>
+                <h4>Reports</h4>
+            </a>
+            <a href="#" class="db-decorate">
+                <span class="material-icons-sharp">settings</span>
+                <h4>Settings</h4>
+            </a>
+            <a href="add_products.php" class="db-decorate">
+                <span class="material-icons-sharp">add</span>
+                <h4>Add Products</h4>
+            </a>
+            <a href="../components/admin_logout.php" onclick="return confirm('Logout from this website?');" class="db-decorate">
+                <span class="material-icons-sharp">logout</span>
+                <h4>Logout</h4>
+            </a>   
+        </div>
+    </nav>
+</header>
